c1c638d8a3e4d593cc356aeb2fa9dc67422fda74
BYPId <a.david.misters@gmail.com>
2024-11-18 20:11:19 +0300

    first project

---
+++ Lab1.2/Lab1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hello
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
+
